59d9d18252ce0716f6c5a2d4e79225b8fbcbcbe8
Andrés Court <andres@andrescourt.com>
2022-10-02 23:40:02 -0500

    added styled components to navbar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,8 +1,19 @@
+
 import { Link } from 'react-router-dom'
 import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap'
+import styled from 'styled-components'
 // import { useAccessToken } from '../context/AccessTokenContext'
 
-export const NavigationBar = () => {
+const NavbarLink = styled(Link)`
+	text-decoration: none;
+	color: #06283D;
+`
+
+const NavDropdownItem = styled(NavDropdown.Item)`
+	color: #06283D;
+`
+
+export const NavigationBar: React.FC = () => {
 	// const accessToken = useAccessToken()
 
 	// const navBar = accessToken?.token?.accessToken ? (
@@ -18,59 +29,59 @@ export const NavigationBar = () => {
 							title='Transactions'
 							id='scp-navbar-nav-transactions'
 						>
-							<NavDropdown.Item>
-								<Link to='/app/transactions/project'>
+							<NavDropdownItem>
+								<NavbarLink to='/app/transactions/project'>
 									Project
-								</Link>
-							</NavDropdown.Item>
-							<NavDropdown.Item>
-								<Link to='/app/transactions/project-budget'>
+								</NavbarLink>
+							</NavDropdownItem>
+							<NavDropdownItem>
+								<NavbarLink to='/app/transactions/project-budget'>
 									Project Budget
-								</Link>
-							</NavDropdown.Item>
-							<NavDropdown.Item>
-								<Link to='/app/transactions/invoice'>
+								</NavbarLink>
+							</NavDropdownItem>
+							<NavDropdownItem>
+								<NavbarLink to='/app/transactions/invoice'>
 									Invoices
-								</Link>
-							</NavDropdown.Item>
+								</NavbarLink>
+							</NavDropdownItem>
 						</NavDropdown>
 						<NavDropdown
 							title='Parameters'
 							id='scp-navbar-nav-parameters'
 						>
-							<NavDropdown.Item>
-								<Link to='/app/parameters/suppliers'>
+							<NavDropdownItem>
+								<NavbarLink to='/app/parameters/suppliers'>
 									Supplier
-								</Link>
-							</NavDropdown.Item>
-							<NavDropdown.Item>
-								<Link to='/app/parameters/budget-items'>
+								</NavbarLink>
+							</NavDropdownItem>
+							<NavDropdownItem>
+								<NavbarLink to='/app/parameters/budget-items'>
 									Budget Items
-								</Link>
-							</NavDropdown.Item>
+								</NavbarLink>
+							</NavDropdownItem>
 						</NavDropdown>
 						<NavDropdown
 							title='Users'
 							id='scp-navbar-nav-users'
 							className='ms-auto'
 						>
-							<NavDropdown.Item>
-								<Link to='/app/users/create'>Create User</Link>
-							</NavDropdown.Item>
-							<NavDropdown.Item>
-								<Link to='/app/users/change-password'>
+							<NavDropdownItem>
+								<NavbarLink to='/app/users/create'>Create User</NavbarLink>
+							</NavDropdownItem>
+							<NavDropdownItem>
+								<NavbarLink to='/app/users/change-password'>
 									Change Password
-								</Link>
-							</NavDropdown.Item>
-							<NavDropdown.Item>
-								<Link to='/app/users/reset-password'>Reset Password</Link>
-							</NavDropdown.Item>
+								</NavbarLink>
+							</NavDropdownItem>
+							<NavDropdownItem>
+								<NavbarLink to='/app/users/reset-password'>Reset Password</NavbarLink>
+							</NavDropdownItem>
 						</NavDropdown>
 					</Nav>
 				</Navbar.Collapse>
 				<ul>
 					<li>
-						<Link to='/app'>App</Link>
+						<NavbarLink to='/app'>App</NavbarLink>
 					</li>
 				</ul>
 			</Container>
